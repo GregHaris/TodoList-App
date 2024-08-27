5879a0e5bfcaacd9d601d1c87bf9e4d80e43f7ae
Grëg Häris <d.gregharis@gmail.com>
2024-08-27 04:48:19 +0100

    add project details creation logic 'project description, duedate, priority, and note' and append to ui

--- src/project.js
+++ src/project.js
@@ -1,6 +1,8 @@
 // Get project title.
 export default function createProject() {
-  const projectCreationSection = document.querySelector("#projectCreationSection");
+  const projectCreationSection = document.querySelector(
+    "#projectCreationSection"
+  );
   const titleInputContainer = createTitleInputContainer();
   projectCreationSection.appendChild(titleInputContainer);
 
@@ -31,18 +33,22 @@ function addTitleBtnEventListener(titleInputContainer, projectCreationSection) {
     const titleInputValue = titleInput.value.trim();
 
     if (titleInputValue) {
-      const projectTitleContainer = createProjectTitleContainer(titleInputValue);
+      const projectTitleContainer =
+        createProjectTitleContainer(titleInputValue);
       projectCreationSection.removeChild(titleInputContainer);
       projectCreationSection.appendChild(projectTitleContainer);
 
-      addProjectTitleEventListeners(projectTitleContainer, projectCreationSection);
+      addProjectTitleEventListeners(
+        projectTitleContainer,
+        projectCreationSection
+      );
     }
   });
 }
 
 function addTitleInputEventListener(titleInputContainer) {
   const titleInput = titleInputContainer.querySelector("input");
-  titleInput.addEventListener("keypress", (e) => {
+  titleInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
       const addTitleBtn = titleInputContainer.querySelector("button");
       addTitleBtn.click();
@@ -74,7 +80,10 @@ function createProjectTitleContainer(titleInputValue) {
   return projectTitleContainer;
 }
 
-function addProjectTitleEventListeners(projectTitleContainer, projectCreationSection) {
+function addProjectTitleEventListeners(
+  projectTitleContainer,
+  projectCreationSection
+) {
   const projectTitle = projectTitleContainer.querySelector(".project-title");
   const editBtn = projectTitleContainer.querySelector(".edit-btn");
   const deleteBtn = projectTitleContainer.querySelector(".delete-btn");
@@ -88,38 +97,241 @@ function addProjectTitleEventListeners(projectTitleContainer, projectCreationSec
     projectCreationSection.removeChild(projectTitleContainer);
   });
 
-  projectTitle.addEventListener("click", () => {
-    const projectContainer = document.querySelector("#projectContainer");
-    const projectHeading = document.createElement("h1");
-    projectHeading.classList.add("project-heading");
-    projectHeading.textContent = projectTitle.textContent;
+  projectTitle.addEventListener(
+    "click",
+    () => {
+      const projectContainer = document.querySelector("#projectContainer");
+      const projectHeading = document.createElement("h1");
+      projectHeading.classList.add("project-heading");
+      projectHeading.textContent = projectTitle.textContent;
 
-    projectContainer.insertBefore(projectHeading, projectContainer.firstChild);
-  });
+      projectContainer.insertBefore(
+        projectHeading,
+        projectContainer.firstChild
+      );
+      createProjectDetails(projectContainer);
+    },
+    { once: true }
+  );
 }
 
-function createProjectDetails () {
-  const descriptionInput = document.createElement("input");
-  descriptionInput.type = "text";
-  descriptionInput.id = "descriptionInput";
-  descriptionInput.placeholder = "Description";
+function createProjectDetails(projectContainer) {
+  const projectDiv = document.createElement("div");
+  projectDiv.id = "project";
+  projectDiv.classList.add("project");
+
+  // factory function to create textarea inputs
+  function createTextAreaContainer(id, heading, placeholder) {
+    const textAreaContainer = document.createElement("div");
+    const textAreaHeadingContainer = document.createElement("div");
+    const textAreaHeading = document.createElement("h4");
+    const textArea = document.createElement("textarea");
+
+    textArea.id = id;
+    textArea.placeholder = placeholder;
+
+    textAreaHeading.textContent = heading;
+
+    textAreaHeadingContainer.appendChild(textAreaHeading);
+    textAreaContainer.append(textAreaHeading, textArea);
+
+    return textAreaContainer;
+  }
+
+  //Add project description
+  const descriptionContainer = createTextAreaContainer(
+    "descriptionInput",
+    "DESCRIPTION",
+    "Describe your project here"
+  );
+  projectDiv.appendChild(descriptionContainer);
+
+  descriptionContainer.addEventListener("change", () => {
+    updateProjectDescription();
+  });
+  function updateProjectDescription() {
+    const projectDescriptionContainer = document.createElement("div");
+    const projectDescription = document.createElement("p");
+    projectDescription.classList.add("project-description");
+    projectDescription.setAttribute("contenteditable", true);
+    projectDescription.textContent = descriptionInput.value;
+    projectDescriptionContainer.appendChild(projectDescription);
+
+    descriptionContainer.removeChild(descriptionInput);
+    descriptionContainer.appendChild(projectDescriptionContainer);
+  }
+
+  // Add project due date
+  const dueDateContainer = document.createElement("div");
+  dueDateContainer.classList.add("due-date-container");
 
   const dueDateInput = document.createElement("input");
   dueDateInput.type = "date";
+  dueDateInput.required = true;
   dueDateInput.id = "dueDateInput";
+
+  const dueDateInputLabel = document.createElement("label");
+  dueDateInputLabel.htmlFor = dueDateInput;
+  dueDateInputLabel.textContent = "Deadline: ";
+
+  dueDateContainer.append(dueDateInputLabel, dueDateInput);
+  projectDiv.appendChild(dueDateContainer);
+
+  // Add project priority
+  const priorityContainer = document.createElement("div");
+  priorityContainer.classList.add("priority-container");
 
   const priorityInput = document.createElement("select");
   priorityInput.id = "priorityInput";
-  const priorityOptions = ["high", "medium", "low"];
-  priorityOptions.forEach(option => {
+
+  const priorityInputLabel = document.createElement("label");
+  priorityInputLabel.htmlFor = priorityInput;
+  priorityInputLabel.textContent = "Project Priority: "
+
+  const priorityOptions = ["Undecided", "High", "Medium", "Low"];
+  priorityOptions.forEach((option) => {
     const optionElement = document.createElement("option");
     optionElement.value = option;
     optionElement.textContent = option;
     priorityInput.appendChild(optionElement);
   });
 
-  const noteInput = document.createElement("textarea");
-  noteInput.id = "noteInput";
-  noteInput.placeholder = "Notes";
+  // Function to set the background color based on the priority value
+  function setPriorityBackgroundColor() {
+    switch (priorityInput.value) {
+      case "High":
+        priorityInput.style.backgroundColor = "#FF0000";
+        break;
+      case "Medium":
+        priorityInput.style.backgroundColor = "#007FFF";
+        break;
+      case "Low":
+        priorityInput.style.backgroundColor = "#228B22";
+        break;
+      default:
+        priorityInput.style.backgroundColor = "#808080";
+        break;
+    }
+  }
+  // Set initial background color
+  setPriorityBackgroundColor();
 
+  // Add event listener to update the background color when the value changes
+  priorityInput.addEventListener("change", setPriorityBackgroundColor);
+
+  priorityContainer.append(priorityInputLabel, priorityInput);
+  projectDiv.appendChild(priorityContainer);
+
+  // Add project Note
+  const noteContainer = createTextAreaContainer(
+    "noteInput",
+    "NOTE",
+    "Insert your notes here"
+  );
+  projectDiv.appendChild(noteContainer);
+
+  noteContainer.addEventListener("change", () => {
+    updateProjectnote();
+  });
+  function updateProjectnote() {
+    const projectNoteContainer = document.createElement("div");
+    const projectNote = document.createElement("p");
+    projectNote.classList.add("project-description");
+    projectNote.setAttribute("contenteditable", true);
+    projectNote.textContent = noteInput.value;
+    projectNoteContainer.appendChild(projectNote);
+
+    noteContainer.removeChild(noteInput);
+    noteContainer.appendChild(projectNoteContainer);
+  }
+
+  projectContainer.appendChild(projectDiv);
 }
+
+// const noteInput = document.createElement("textarea");
+// noteInput.id = "noteInput";
+// noteInput.placeholder = "Notes";
+
+// projectContainer.appendChild(descriptionInput);
+// projectContainer.appendChild(dueDateInput);
+// projectContainer.appendChild(priorityInput);
+// projectContainer.appendChild(noteInput);
+
+// // Add event listeners to each input field
+// descriptionInput.addEventListener("input", () => {
+//   displayProject(
+//     descriptionInput.value,
+//     dueDateInput.value,
+//     priorityInput.value,
+//     noteInput.value,
+//     projectContainer
+//   );
+// });
+
+// dueDateInput.addEventListener("input", () => {
+//   displayProject(
+//     descriptionInput.value,
+//     dueDateInput.value,
+//     priorityInput.value,
+//     noteInput.value,
+//     projectContainer
+//   );
+// });
+
+// priorityInput.addEventListener("change", () => {
+//   displayProject(
+//     descriptionInput.value,
+//     dueDateInput.value,
+//     priorityInput.value,
+//     noteInput.value,
+//     projectContainer
+//   );
+// });
+
+// noteInput.addEventListener("input", () => {
+//   displayProject(
+//     descriptionInput.value,
+//     dueDateInput.value,
+//     priorityInput.value,
+//     noteInput.value,
+//     projectContainer
+//   );
+// });
+// }
+
+// // Function to display project on the UI
+// function displayProject(description, dueDate, priority, note, projectContainer) {
+//   const projectDiv = document.createElement("div");
+//   projectDiv.id = "project";
+
+//   const projectDescription = document.createElement("p");
+//   projectDescription.classList.add("project-description");
+//   projectDescription.textContent = description;
+
+//   const projectDueDate = document.createElement("p");
+//   projectDueDate.classList.add("project-due-date");
+//   projectDueDate.textContent = dueDate;
+
+//   const projectPriority = document.createElement("span");
+//   projectPriority.classList.add("project-priority");
+//   projectPriority.textContent = priority;
+//   switch (priority) {
+//     case "high":
+//       projectPriority.style.backgroundColor = "red";
+//       break;
+//     case "low":
+//       projectPriority.style.backgroundColor = "green";
+//       break;
+//     default:
+//       projectPriority.style.backgroundColor = "gray";
+//       break;
+//   }
+//   projectPriority.style.color = "white";
+
+//   const projectNote = document.createElement("p");
+//   projectNote.classList.add("project-note");
+//   projectNote.textContent = note;
+
+//   projectDiv.append(projectDescription, projectDueDate, projectPriority, projectNote);
+//   projectContainer.appendChild(projectDiv);
+// }
